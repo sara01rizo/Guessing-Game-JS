0fa3e40bddf240d0c6773827cfec54b489332b50
Sara Pulido <sara01rizo@gmail.com>
2021-10-29 14:51:24 +0200

    Loop created includes parseInt

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ while (!maximum) {
 }
 
 const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+let guess = parseInt(prompt("Enter your first guess!"));
+let attemps = 1;
+
+while (parseInt(guess) !== targetNum) {
+  if (guess === "q") break;
+  attemps++;
+  if (guess > targetNum) {
+    guess = prompt("Too high! Enter a new guess:");
+  } else {
+    guess = prompt("Too low! Enter a new guess:");
+  }
+}
